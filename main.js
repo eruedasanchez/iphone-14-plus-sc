aebbbe22ca6414d4cd6b9c50ff726940c50e6633
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-06-10 12:38:07 -0300

    Add total products to cart icon

--- main.js
+++ main.js
@@ -19,3 +19,17 @@ minusBtn.addEventListener('click', () => {
     userInput.value = userInputNumber;
 });
 
+/* Agregar el total de productos al icono del carrito cuando se presiona el btn agregar al carrito */
+
+const addToCartBtn = document.querySelector('.details__button');
+let cartNotification = document.querySelector('.header-container__carrito--notif');
+
+addToCartBtn.addEventListener('click', () => {
+    let lastValue = parseInt(cartNotification.innerText);
+    lastValue += userInputNumber;                           // suma la cantidad actual y la anterior (no los pisa)
+    cartNotification.innerText = lastValue;
+    cartNotification.style.display = 'block';              // .style cambia los estilos y .display de la prop display unicamente 
+});
+
+
+
